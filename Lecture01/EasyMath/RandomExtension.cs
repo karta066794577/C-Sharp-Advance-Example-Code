b0bc75d401b6844abc650305780cfdaf46a77a5c
劉承鑫 <38624554+karta066794577@users.noreply.github.com>
2018-09-24 12:58:09 +0800

    新增 Lecture 01
    基礎語法複習

---
+++ Lecture01/EasyMath/RandomExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMath
+{
+    public static class RandomExtension
+    {
+        /// <summary>
+        /// 隨機產生一個浮點數，範圍在 min 和 max 之間。
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="min">最小值</param>
+        /// <param name="max">最大值</param>
+        /// <returns></returns>
+        public static double NextDouble(this Random random, double min, double max)
+        {
+            return min + (max - min) * random.NextDouble();
+        }
+    }
+}
---
+++ Lecture01/EasyMath/Vec2.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMath
+{
+    public class Vec2 : IComparable<Vec2>, IComparable
+    {
+        protected static Random random;
+        public static Order order { get; set; }
+        public static SelectedField selectedField { get; set; }
+        private double x;
+        private double y;
+
+        public double X { get => this.x; set => this.x = value; }
+        public double Y { get => this.y; set => this.y = value; }
+
+        public Vec2(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Vec2() : this(0.0, 0.0)
+        {
+        }
+
+        public virtual double Length
+        {
+            get { return Math.Sqrt(this.X * this.X + this.Y * this.Y); }
+        }
+
+        public int OrderFactor
+        {
+            get { return order == Order.ASC ? 1 : -1; }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Vec2)
+            {
+                Vec2 other = obj as Vec2;
+                return ResultOfCompareTo(other);
+            }
+            else
+                return 0;
+        }
+
+        public int CompareTo(Vec2 other)
+        {
+            return ResultOfCompareTo(other);
+        }
+
+        public int Compare(double a, double b)
+        {
+            int result = 0;
+            if (a > b)
+                result = 1;
+            else if (a == b)
+                result = 0;
+            else
+                result = -1;
+
+            return result * OrderFactor;
+        }
+
+        public int ResultOfCompareTo(Vec2 other)
+        {
+            switch (selectedField)
+            {
+                case SelectedField.X:
+                    return Compare(this.x, other.x);
+
+                case SelectedField.Y:
+                    return Compare(this.y, other.y);
+
+                case SelectedField.Length:
+                default:
+                    return Compare(this.Length, other.Length);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("(x, y) = ({0}, {1}) Length = {2}", X, Y, Length.ToString("#.##"));
+        }
+
+        public static Vec2 Generate(double min, double max)
+        {
+            if (Vec2.random == null)
+                Vec2.random = new Random();
+            double x = random.NextDouble(min, max);
+            double y = random.NextDouble(min, max);
+            return new Vec2(x, y);
+        }
+
+        public static Vec2[] Generate(double min, double max, int count)
+        {
+            Vec2[] vectors = new Vec2[count];
+            for (int index = 0; index < vectors.Length; index++)
+                vectors[index] = Vec2.Generate(min, max);
+            return vectors;
+        }
+
+        public static void WriteLine(Vec2 v)
+        {
+            Console.WriteLine(v);
+        }
+
+        public static void WriteLine(Vec2[] vectors)
+        {
+            foreach (var vector in vectors)
+                Vec2.WriteLine(vector);
+        }
+
+        public static string ToString(Vec2[] vectors)
+        {
+            string result = "";
+            foreach (var vector in vectors)
+                result += vector.ToString() + "\n";
+            return result;
+        }
+    }
+}
